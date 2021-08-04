8428e624f6a8b9810115fe4a5c226aa21466ce51
Saddam Satria <ardhisaddam@gmail.com>
2021-08-04 12:32:45 +0700

    first commit

--- src/store.js
+++ src/store.js
@@ -2,4 +2,4 @@ import {createStore,compose,applyMiddleware}  from 'redux'
 import thunk from 'redux-thunk'
 import {reducers} from './reducers'
 
-export const store = createStore(reducers,compose(applyMiddleware(thunk),window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()))
+export const store = createStore(reducers,compose(applyMiddleware(thunk)))
